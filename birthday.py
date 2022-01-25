4dcf9ec6d26e015bacdce552aaf63809032b32b5
Tran <Khang.Tran@rjuhsd.int>
2022-01-25 12:50:10 -0800

    space jam dvd space jam dvd space jam dvd space jam dvd space jam dvd space jam dvd space jam dvd

--- birthday.py
+++ birthday.py
@@ -1,8 +1,10 @@
-print("sup dude")
+from calendar import month
 
-for i in range(50):
-    print("owen is a butt")
 
-option = input("Test")
+day = input("What is the day that you where born?")
+m = input("What is the month that you where born?")
+year = input("What is the year that you where born?")
+
+print("You were born on: ", m, day, year)
 
 
